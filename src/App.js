1096631870f571bb937630e96333883189f49829
Kurnia <kurniasuryani@gmail.com>
2021-03-15 00:17:21 +0100

    add npm scripts, eslint, Prettier, gitignore

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const Pet = ({ name, animal, breed }) => {
     return React.createElement("div", {}, [
         React.createElement("h1", {}, name),
         React.createElement("h2", {}, animal),
-        React.createElement("h2", {}, breed)
+        React.createElement("h2", {}, breed),
     ]);
 };
 
 const App = () => {
-    return React.createElement(
-        "div", {}, [
-            React.createElement("h1", {}, "Adopt Me!"),
-            React.createElement(Pet, {
-                name: "Luna",
-                animal: "Dog",
-                breed: "Havanese"
-            }),
-            React.createElement(Pet, {
-                name: "Pepper",
-                animal: "Bird",
-                breed: "Cockatiel"
-            }),
-            React.createElement(Pet, {
-                name: "Meng",
-                animal: "Cat",
-                breed: "Persian"
-            })
-        ]
-    );
+    return React.createElement("div", {}, [
+        React.createElement("h1", {}, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese",
+        }),
+        React.createElement(Pet, {
+            name: "Pepper",
+            animal: "Bird",
+            breed: "Cockatiel",
+        }),
+        React.createElement(Pet, {
+            name: "Meng",
+            animal: "Cat",
+            breed: "Persian",
+        }),
+    ]);
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
