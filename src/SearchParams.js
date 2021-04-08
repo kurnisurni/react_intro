cb108c47d6abda22879ccb891fac62b7cbd047ce
Kurnia <kurniasuryani@gmail.com>
2021-04-09 00:31:46 +0200

    custom hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,53 +1,40 @@
-import React, { useState } from 'react';
-import { ANIMALS } from '@frontendmasters/pet';
-import useDropdown from './useDropdown';
+import React, { useState, useEffect } from "react";
+import pet, { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
-    const [location, setLocation] = useState("Seattle, WA");
-    // const [animal, setAnimal] = useState("dog");
-    // const [breed, setBreed] = useState("");
-    const [breeds, setBreeds] = useState([]);
-    const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
-    const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+  const [location, updateLocation] = useState("Seattle, WA");
+  const [breeds, updateBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown, updateBreed] = useDropdown("Breed", "", breeds);
 
-    return (
-        <div className="search-params">
-            <form>
-                <label htmlFor="location">
-                    Location
-                    <input id="location" value={location} 
-                    placeholder="Location" 
-                    onChange={e => setLocation(e.target.value)} />
-                </label>
-                <label htmlFor="animal">
-                    Animal
-                    <select id="animal" value="animal" 
-                    onChange={e => setAnimal(e.target.value)}
-                    onBlur={e => setAnimal(e.target.value)}>
-                    <option>All</option>
-                    { ANIMALS.map(animal => (
-                    <option key={animal} value={animal}>{animal}</option>
-                    ))}
-                    </select>
-                </label>
-                <label htmlFor="breed">
-                    Breed
-                    <select id="breed" value={breed}
-                    onChange={e => setBreed(e.target.value)}
-                    onBlur={e => setBreed(e.target.value)}
-                    disabled={breeds.length === 0}
-                    >
-                        <option>All</option>
-                        {breeds.map(breedString => (
-                            <option key={breedString} value={breedString}>{breedString}</option>
-                        ))}
-                    </select>
-                </label>
-                <button>Submit</button>
-            </form>
+  useEffect(() => {
+    updateBreeds([]);
+    updateBreed("");
+    pet.breeds(animal).then(({ breeds }) => {
+      const breedStrings = breeds.map(({ name }) => name);
+      updateBreeds(breedStrings);
+    }, console.error);
+  }, [animal]);
 
-        </div>
-    );
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            placeholder="Location"
+            onChange={e => updateLocation(e.target.value)}
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
 };
 
 export default SearchParams;
