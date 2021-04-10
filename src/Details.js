5830a59c0447512a43c6edff08c66f973d9aa740
Kurnia <kurniasuryani@gmail.com>
2021-04-10 16:38:46 +0200

    rendering pet details component

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,47 @@
 import React from "react";
+import pet from  "@frontendmasters/pet";
 
-const Details = () => {
-    return <h1>hi lol</h1>
+class Details extends React.Component {
+    constructor(props) {  //do this constructor after component below, to set it to be a loading state
+        super(props);
+
+        this.state = {
+          loading:true
+        }
+    }
+    componentDidMount () {
+        pet.animal(this.props.id).then(({ animal }) => {  //this props is info that you get from parent class that's handed down to you
+            this.setState({
+                name: animal.name,
+                animal: animal.type,
+                location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+                description: animal.description,
+                media: animal.photos,
+                breed: animal.breeds.primary, 
+                loading: false
+            });
+        }, console.error);
+    }
+
+    render () {
+      if (this.state.loading) {
+        return <h1>loading...</h1>
+      }
+
+      const { animal, breed, location, description, name } = this.state;
+
+      return (
+        <div className="details">
+          <div>
+            <h1>{name}</h1>
+            <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            <button>Adopt {name}</button>
+            <p>{description}</p>
+          </div>
+        </div>
+      )
+
+    }
 }
 
 export default Details
