fcd8428f3c5736f5629974a896df7bfed468c226
Kurnia <kurniasuryani@gmail.com>
2021-04-10 15:21:12 +0200

    reformatting Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,14 +1,31 @@
 import React from "react";
 
-const Pet = props => {
+// const Pet = props => {
 
-return (
-    <div>
-        <h1>Name: {props.name.toUpperCase()}</h1>
-        <h2>{props.animal}</h2>
-        <h2>{props.breed}</h2>
-    </div>
-    );
-};
+// return (
+//     <div>
+//         <h1>Name: {props.name.toUpperCase()}</h1>
+//         <h2>{props.animal}</h2>
+//         <h2>{props.breed}</h2>
+//     </div>
+//     );
+// };
 
-export default Pet;
+export default function Pet ({name, animal, breed, media, location, id}) {
+    let hero = 'http://placecorgi.com/300/300';
+    if(media.length) {
+        hero = media[0].small;
+    }
+
+    return (
+        <a href={`/details/${id}`} className="pet">
+            <div className="image-container">
+                <img src= {hero} alt={name}/>
+            </div>
+            <div className="info">
+                <h1>{name}</h1>
+                <h2>{`${animal} - ${breed} - ${location}`}</h2>
+            </div>
+        </a>
+    )
+}
